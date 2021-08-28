8a8f4573717b3dd71e7ea5c22eb75c8be88482d5
William DeWitt <wsdewitt@gmail.com>
2021-08-27 17:31:45 -0700

    black format all python code

--- mutyper/cli.py
+++ mutyper/cli.py
@@ -19,7 +19,7 @@ from mutyper import ancestor
 
 def setup_ancestor(args):
     """utility for initializing an Ancestor object for use in different '
-    'subroutines"""
+    'subroutines."""
     return ancestor.Ancestor(
         args.fasta,
         k=args.k,
@@ -33,7 +33,7 @@ def setup_ancestor(args):
 
 # mrv addition, whole function
 def is_compressed(file):
-    """returns true if file is compressed"""
+    """returns true if file is compressed."""
     f = open(file, "rb")
     # The first two bytes of a gzip file are: 1f 8b
     compressed = f.read(2) == b"\x1f\x8b"
@@ -44,9 +44,9 @@ def is_compressed(file):
 
 def copy_fasta(file, outfile):
     """copy fasta file to outfile and decompress if needed.
-    This is necessary because pyfaidx does not support mutable compressed
-    so we cannot just copy the input if compressed.
-        pyfaidx.UnsupportedCompressionFormat: BGZF compressed FASTA is not supported for MutableFastaRecord. Please decompress your FASTA file.
+
+    This is necessary because pyfaidx does not support mutable
+    compressed, so we cannot just copy the input if compressed.
     """
     # outfile cannot be compressed
     if outfile.endswith(".gz"):
@@ -62,7 +62,7 @@ def copy_fasta(file, outfile):
 
 
 def ancestral_fasta(args):
-    """subroutine for ancestor subcommand"""
+    """subroutine for ancestor subcommand."""
     # single chromosome fasta file for reference genome
     ref = pyfaidx.Fasta(args.reference, read_ahead=10000)
     # make a copy to build our ancestor for this chromosome
@@ -147,7 +147,7 @@ def ancestral_fasta(args):
 
 
 def variants(args):
-    """subroutine for variants subcommand"""
+    """subroutine for variants subcommand."""
     ancestor = setup_ancestor(args)
 
     vcf = cyvcf2.VCF(args.vcf)
@@ -215,7 +215,7 @@ def variants(args):
 
 
 def targets(args):
-    """subroutine for targets subcommand"""
+    """subroutine for targets subcommand."""
     ancestor = setup_ancestor(args)
 
     if args.bed == "-":
@@ -231,7 +231,7 @@ def targets(args):
 
 
 def spectra(args):
-    """subroutine for spectra subcommand"""
+    """subroutine for spectra subcommand."""
     vcf = cyvcf2.VCF(args.vcf, gts012=True)
 
     if args.population:
@@ -274,7 +274,7 @@ def spectra(args):
 
 
 def ksfs(args):
-    """subroutine for ksfs subcommand"""
+    """subroutine for ksfs subcommand."""
     vcf = cyvcf2.VCF(args.vcf)
 
     ksfs_data = defaultdict(lambda: Counter())
