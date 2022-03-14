387228286475e054ec3360a08de83f5ad429c658
William DeWitt <wsdewitt@gmail.com>
2022-03-13 21:57:26 -0700

    more cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,7 +16,44 @@ def test_spectra(capsys):
     captured = capsys.readouterr()
     df = pd.read_csv(io.StringIO(captured.out), sep="\t", index_col=0)
     df_target = pd.DataFrame(
-        {"ACA>ATA": [0, 0], "ACC>ATC": [1, 0], "ACG>ATG": [1, 0], "ACT>ATT": [2, 0]},
+        {"ACA>ATA": [0, 1], "ACC>ATC": [1, 0], "ACG>ATG": [1, 1], "ACT>ATT": [2, 0]},
+        index=pd.Index(["sample1", "sample2"], name="sample"),
+    )
+    pd.testing.assert_frame_equal(df, df_target)
+
+
+def test_spectra_randomize(capsys):
+    args = argparse.Namespace(
+        vcf="tests/test_data/snps.vcf", population=False, randomize=True
+    )
+    cli.spectra(args)
+    captured = capsys.readouterr()
+    df = pd.read_csv(io.StringIO(captured.out), sep="\t", index_col=0)
+    # there are two possibilities due to haplotype randomization
+    df_target1 = pd.DataFrame(
+        {"ACA>ATA": [0, 1], "ACC>ATC": [1, 0], "ACG>ATG": [1, 0], "ACT>ATT": [1, 0]},
+        index=pd.Index(["sample1", "sample2"], name="sample"),
+    )
+    df_target2 = pd.DataFrame(
+        {"ACA>ATA": [0, 1], "ACC>ATC": [1, 0], "ACG>ATG": [0, 1], "ACT>ATT": [1, 0]},
+        index=pd.Index(["sample1", "sample2"], name="sample"),
+    )
+
+    try:
+        pd.testing.assert_frame_equal(df, df_target1)
+    except AssertionError:
+        pd.testing.assert_frame_equal(df, df_target2)
+
+
+def test_spectra_haploid(capsys):
+    args = argparse.Namespace(
+        vcf="tests/test_data/snps.haploid.vcf", population=False, randomize=False
+    )
+    cli.spectra(args)
+    captured = capsys.readouterr()
+    df = pd.read_csv(io.StringIO(captured.out), sep="\t", index_col=0)
+    df_target = pd.DataFrame(
+        {"ACA>ATA": [0, 1], "ACC>ATC": [0, 0], "ACG>ATG": [1, 1], "ACT>ATT": [1, 0]},
         index=pd.Index(["sample1", "sample2"], name="sample"),
     )
     pd.testing.assert_frame_equal(df, df_target)
@@ -30,7 +67,7 @@ def test_spectra_missing_gts(capsys, caplog):
     captured = capsys.readouterr()
     df = pd.read_csv(io.StringIO(captured.out), sep="\t", index_col=0)
     df_target = pd.DataFrame(
-        {"ACA>ATA": [0, 0], "ACC>ATC": [1, 0], "ACG>ATG": [1, 0], "ACT>ATT": [1, 0]},
+        {"ACA>ATA": [0, 1], "ACC>ATC": [1, 0], "ACG>ATG": [1, 1], "ACT>ATT": [1, 0]},
         index=pd.Index(["sample1", "sample2"], name="sample"),
     )
     pd.testing.assert_frame_equal(df, df_target)
@@ -44,9 +81,9 @@ def test_ksfs(capsys):
     df = pd.read_csv(io.StringIO(captured.out), sep="\t", index_col=0)
     df_target = pd.DataFrame(
         {
-            "ACA>ATA": [0, 0, 0],
+            "ACA>ATA": [1, 0, 0],
             "ACC>ATC": [1, 0, 0],
-            "ACG>ATG": [1, 0, 0],
+            "ACG>ATG": [0, 1, 0],
             "ACT>ATT": [0, 1, 0],
         },
         index=pd.Index([1, 2, 3], name="sample_frequency"),
