6ecbd6481e5c2184f9ac6f07cb6a02398ba8c7e1
William DeWitt <wsdewitt@gmail.com>
2022-03-13 21:57:09 -0700

    avoid problematic gt_types attribute in cyvcf2

--- mutyper/cli.py
+++ mutyper/cli.py
@@ -9,7 +9,6 @@ import signal
 import numpy as np
 from shutil import copyfile, copyfileobj
 import pyfaidx
-from random import choice
 from pyliftover import LiftOver
 from Bio.Seq import reverse_complement
 import logging
@@ -58,6 +57,19 @@ def copy_fasta(file, outfile):
             copyfileobj(f_in, f_out)
     else:
         copyfile(file, outfile)
+
+
+def iterate_with_ambiguity_warning(vcf):
+    """In several places we want to check for genotype ambiguity as we iterate
+    over vcf variants, so we define this wrapper generator."""
+    for variant in vcf:
+        yield variant
+        if variant.num_unknown:
+            logging.warning(
+                "Ambiguous genotypes found! Continuing by assuming reference genotypes for these variants."
+            )
+            break
+    yield from vcf
 
 
 def ancestral_fasta(args):
@@ -231,13 +243,14 @@ def targets(args):
 
 def spectra(args):
     """subroutine for spectra subcommand."""
-    vcf = cyvcf2.VCF(args.vcf, gts012=True)
+    vcf = cyvcf2.VCF(args.vcf, strict_gt=True)
 
     if args.population:
         spectra_data = Counter()
 
-        for variant in vcf:
-            spectra_data[variant.INFO["mutation_type"]] += 1
+        for variant in iterate_with_ambiguity_warning(vcf):
+            if variant.aaf:
+                spectra_data[variant.INFO["mutation_type"]] += 1
 
         spectra = pd.DataFrame(spectra_data, ["population"]).reindex(
             sorted(spectra_data), axis="columns"
@@ -249,30 +262,16 @@ def spectra(args):
 
     else:
         spectra_data = defaultdict(lambda: np.zeros_like(vcf.samples, dtype=int))
-        seen_ambiguous = False
         if args.randomize:
-            for variant in vcf:
-                random_haplotype = choice(
-                    [x for x, y in enumerate(variant.gt_types) for _ in range(y)]
-                )
+            for variant in iterate_with_ambiguity_warning(vcf):
+                counts = np.array([gt.count(1) for gt in variant.genotypes])
+                rng = np.random.default_rng()
+                random_haplotype = rng.choice(len(counts), p=counts / counts.sum())
                 spectra_data[variant.INFO["mutation_type"]][random_haplotype] += 1.0
         else:
-            for variant in vcf:
-                if variant.ploidy == 1:
-                    # haploid ALT are coded as 2 (homozygous ALT)
-                    variant.gt_types[variant.gt_types == 2] = 1
-                # from the cyvcf2 docs:
-                #   gt_types is array of 0,1,2,3==HOM_REF, HET, UNKNOWN, HOM_ALT
-                #   gts012 (bool) – if True, then gt_types will be 0=HOM_REF, 1=HET, 2=HOM_ALT, 3=UNKNOWN. If False, 3, 2 are flipped.
-                # but for our case unknown should be 0, not 3.
-                ambiguous_genotypes = variant.gt_types == 3
-                if not seen_ambiguous and any(ambiguous_genotypes):
-                    logging.warning(
-                        "Ambiguous genotypes found! Continuing by assuming reference genotypes for these variants."
-                    )
-                    seen_ambiguous = True
-                variant.gt_types[ambiguous_genotypes] = 0
-                spectra_data[variant.INFO["mutation_type"]] += variant.gt_types
+            for variant in iterate_with_ambiguity_warning(vcf):
+                counts = np.array([gt.count(1) for gt in variant.genotypes])
+                spectra_data[variant.INFO["mutation_type"]] += counts
 
         spectra = pd.DataFrame(spectra_data, vcf.samples).reindex(
             sorted(spectra_data), axis="columns"
